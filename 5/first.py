83256f182f4f9a47ecbd185dea6c9d6f9f8ed230
Edward Dale <scompt@scompt.com>
2019-12-05 21:26:47 +0100

    Day 5, Part 1
    Uff

--- 5/first.py
+++ 5/first.py
@@ -6,6 +6,7 @@ from textwrap import dedent
 class HaltException(Exception):
     pass
 
+
 def read_input(inp):
     """
     >>> read_input('1,2,3,4,5')
@@ -19,32 +20,63 @@ def read_input(inp):
     >>> read_input(dedent(inp).strip())
     [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
     """
-    return [int(i.strip()) for i in inp.split(',')]
+    return [int(i.strip()) for i in inp.split(",")]
 
-def op_add(memory, pc):
-    operand1 = memory[memory[pc+1]]
-    operand2 = memory[memory[pc+2]]
+
+def read_memory(memory, val, immediate_mode):
+    if immediate_mode:
+        return val
+    else:
+        return memory[val]
+
+
+def parse_modes(modes):
+    return [bool(int(i)) for i in f"{modes:04d}"[::-1]]
+
+
+def op_add(memory, modes, pc, inputs):
+    operand1 = read_memory(memory, memory[pc + 1], modes[0])
+    operand2 = read_memory(memory, memory[pc + 2], modes[1])
     result = operand1 + operand2
-    memory[memory[pc+3]] = result
+    memory[memory[pc + 3]] = result
     return 4
 
-def op_mul(memory, pc):
-    operand1 = memory[memory[pc+1]]
-    operand2 = memory[memory[pc+2]]
+
+def op_mul(memory, modes, pc, inputs):
+    operand1 = read_memory(memory, memory[pc + 1], modes[0])
+    operand2 = read_memory(memory, memory[pc + 2], modes[1])
     result = operand1 * operand2
-    memory[memory[pc+3]] = result
+    memory[memory[pc + 3]] = result
     return 4
 
-def op_halt(memory, pc):
+
+def op_load(memory, modes, pc, inputs):
+    loc = memory[pc + 1]
+    val = int(inputs.pop(0))
+    memory[loc] = val
+    return 2
+
+
+def op_print(memory, modes, pc, inputs):
+    loc = memory[pc + 1]
+    print(memory[loc])
+    return 2
+
+
+def op_halt(memory, modes, pc, inputs):
     raise HaltException()
 
-operations = {
-        1: op_add,
-        2: op_mul,
-        99: op_halt,
-        }
 
-def process(memory):
+operations = {
+    1: op_add,
+    2: op_mul,
+    3: op_load,
+    4: op_print,
+    99: op_halt,
+}
+
+
+def process(memory, inputs):
     """
     >>> process([1, 0, 0, 2, 99])
     [1, 0, 2, 2, 99]
@@ -65,8 +97,10 @@ def process(memory):
         intcode = memory[pc]
 
         try:
-            op = operations[intcode]
-            skip_len = op(memory, pc)
+            opcode = intcode % 100
+            modes = parse_modes(intcode // 100)
+            op = operations[opcode]
+            skip_len = op(memory, modes, pc, inputs)
             pc += skip_len
 
         except HaltException:
@@ -76,22 +110,10 @@ def process(memory):
             raise Exception("Unexpected intcode: %s" % intcode)
 
 
-def search(memory, target):
-    orig = memory
-
-    for noun in range(0, 100):
-        for verb in range(0, 100):
-            memory = orig[:]
-            memory[1] = noun
-            memory[2] = verb
-            out = process(memory)
-
-            if out[0] == target:
-                return (noun, verb)
-
-
 if __name__ == "__main__":
     import sys
-    memory = read_input(','.join(sys.stdin.readlines()))
-    noun, verb = search(memory, 19690720)
-    print(100 * noun + verb)
+
+    lines = sys.stdin.readlines()
+    memory = read_input(lines[0])
+    inputs = lines[1:]
+    process(memory, inputs)
