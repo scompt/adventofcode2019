eb8ede4e9ac8f551b6a44866978b1834bf41c6d5
Edward Dale <scompt@scompt.com>
2019-12-07 10:40:42 +0100

    Day 7, Part 2

--- 7/second.py
+++ 7/second.py
@@ -171,44 +171,63 @@ def calculate_thruster(program, phase_sequence):
     >>> phase_sequence = '10432'
     >>> calculate_thruster(program, phase_sequence)
     65210
+
+    >>> program = read_input('3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5')
+    >>> phase_sequence = '98765'
+    >>> calculate_thruster(program, phase_sequence)
+    139629729
+    >>> program = read_input('3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10')
+    >>> phase_sequence = '97856'
+    >>> calculate_thruster(program, phase_sequence)
+    18216
     """
+    amp_count = len(phase_sequence)
 
-    computer_threads = []
-    ultimate_input_queue = queue.Queue()
-    input_queue = ultimate_input_queue
+    queues = [queue.Queue() for i in range(amp_count)]
+    computers = [
+        IntcodeComputer(program, queues[i], queues[(i + 1) % amp_count])
+        for i in range(amp_count)
+    ]
+    threads = [threading.Thread(target=computer.run) for computer in computers]
 
-    for amp in range(len(phase_sequence)):
-        phase = int(phase_sequence[amp])
-        input_queue.put(phase)
-        output_queue = queue.Queue()
+    for i in range(amp_count):
+        phase = int(phase_sequence[i])
+        queues[i].put(phase)
 
-        computer = IntcodeComputer(program, input_queue, output_queue)
-        thread = threading.Thread(target=computer.run)
-        computer_threads.append(thread)
+    queues[0].put(0)
 
-        input_queue = output_queue
-
-    ultimate_input_queue.put(0)
-
-    for t in computer_threads:
+    for t in threads:
         t.start()
+    for t in threads:
+        t.join()
 
-    return input_queue.get()
+    return queues[0].get()
 
 
-def optimize_thruster(program):
+def optimize_thruster(program, with_feedback):
     """
     >>> program = read_input('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0')
-    >>> optimize_thruster(program)
+    >>> optimize_thruster(program, False)
     43210
     >>> program = read_input('3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0')
-    >>> optimize_thruster(program)
+    >>> optimize_thruster(program, False)
     54321
     >>> program = read_input('3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0')
-    >>> optimize_thruster(program)
+    >>> optimize_thruster(program, False)
     65210
+    
+    >>> program = read_input('3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5')
+    >>> optimize_thruster(program, True)
+    139629729
+    >>> program = read_input('3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10')
+    >>> optimize_thruster(program, True)
+    18216
     """
-    phases = list(permutations(range(5)))
+    if with_feedback:
+        phases = list(permutations([5, 6, 7, 8, 9]))
+    else:
+        phases = list(permutations([0, 1, 2, 3, 4]))
+
     thrusts = list(map(lambda p: calculate_thruster(program, p), phases))
     max_index, max_thrust = max(enumerate(thrusts), key=operator.itemgetter(1))
     return max_thrust
@@ -219,5 +238,5 @@ if __name__ == "__main__":
 
     lines = sys.stdin.readlines()
     memory = read_input(lines[0])
-    max_thrust = optimize_thruster(memory)
+    max_thrust = optimize_thruster(memory, True)
     print(max_thrust)
