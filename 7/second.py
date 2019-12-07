3f586bf0382e60ac8d9b14f53f6883eee26f89a5
Edward Dale <scompt@scompt.com>
2019-12-07 10:09:07 +0100

    Convert to use threads

--- 7/second.py
+++ 7/second.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 import operator
+import queue
+import threading
 from itertools import permutations
 from textwrap import dedent
-
-
-class HaltException(Exception):
-    pass
 
 
 def read_input(inp):
@@ -25,135 +23,143 @@ def read_input(inp):
     return [int(i.strip()) for i in inp.split(",")]
 
 
-def read_memory(memory, val, immediate_mode):
-    if immediate_mode:
-        return val
-    else:
-        return memory[val]
-
-
 def parse_modes(modes):
     return [bool(int(i)) for i in f"{modes:04d}"[::-1]]
 
 
-def op_add(memory, modes, pc, inputs, outputs):
-    operand1 = read_memory(memory, memory[pc + 1], modes[0])
-    operand2 = read_memory(memory, memory[pc + 2], modes[1])
-    result = operand1 + operand2
-    memory[memory[pc + 3]] = result
-    return pc + 4
+class HaltException(Exception):
+    pass
 
 
-def op_mul(memory, modes, pc, inputs, outputs):
-    operand1 = read_memory(memory, memory[pc + 1], modes[0])
-    operand2 = read_memory(memory, memory[pc + 2], modes[1])
-    result = operand1 * operand2
-    memory[memory[pc + 3]] = result
-    return pc + 4
+class IntcodeComputer(object):
+    def __init__(self, program, input_queue=queue.Queue(), output_queue=queue.Queue()):
+        self.memory = program[:]
+        self.pc = 0
+        self.input_queue = input_queue
+        self.output_queue = output_queue
 
+        self.operations = {
+            1: self.op_add,
+            2: self.op_mul,
+            3: self.op_load,
+            4: self.op_print,
+            5: self.op_jump_if_true,
+            6: self.op_jump_if_false,
+            7: self.op_less_than,
+            8: self.op_equals,
+            99: self.op_halt,
+        }
 
-def op_load(memory, modes, pc, inputs, outputs):
-    loc = memory[pc + 1]
-    val = int(inputs.pop(0))
-    memory[loc] = val
-    return pc + 2
+    def read_memory(self, val, immediate_mode):
+        if immediate_mode:
+            return val
+        else:
+            return self.memory[val]
 
+    def op_add(self, modes):
+        operand1 = self.read_memory(self.memory[self.pc + 1], modes[0])
+        operand2 = self.read_memory(self.memory[self.pc + 2], modes[1])
+        result = operand1 + operand2
+        self.memory[self.memory[self.pc + 3]] = result
+        return self.pc + 4
 
-def op_print(memory, modes, pc, inputs, outputs):
-    loc = memory[pc + 1]
-    outputs.append(memory[loc])
-    return pc + 2
+    def op_mul(self, modes):
+        operand1 = self.read_memory(self.memory[self.pc + 1], modes[0])
+        operand2 = self.read_memory(self.memory[self.pc + 2], modes[1])
+        result = operand1 * operand2
+        self.memory[self.memory[self.pc + 3]] = result
+        return self.pc + 4
 
+    def op_load(self, modes):
+        loc = self.memory[self.pc + 1]
+        val = int(self.input_queue.get())
+        self.memory[loc] = val
+        return self.pc + 2
 
-def op_jump_if_true(memory, modes, pc, inputs, outputs):
-    operand1 = read_memory(memory, memory[pc + 1], modes[0])
-    operand2 = read_memory(memory, memory[pc + 2], modes[1])
-    if operand1 > 0:
-        return operand2
-    else:
-        return pc + 3
+    def op_print(self, modes):
+        loc = self.memory[self.pc + 1]
+        self.output_queue.put(self.memory[loc])
+        return self.pc + 2
 
+    def op_jump_if_true(self, modes):
+        operand1 = self.read_memory(self.memory[self.pc + 1], modes[0])
+        operand2 = self.read_memory(self.memory[self.pc + 2], modes[1])
+        if operand1 > 0:
+            return operand2
+        else:
+            return self.pc + 3
 
-def op_jump_if_false(memory, modes, pc, inputs, outputs):
-    operand1 = read_memory(memory, memory[pc + 1], modes[0])
-    operand2 = read_memory(memory, memory[pc + 2], modes[1])
-    if operand1 == 0:
-        return operand2
-    else:
-        return pc + 3
+    def op_jump_if_false(self, modes):
+        operand1 = self.read_memory(self.memory[self.pc + 1], modes[0])
+        operand2 = self.read_memory(self.memory[self.pc + 2], modes[1])
+        if operand1 == 0:
+            return operand2
+        else:
+            return self.pc + 3
 
+    def op_less_than(self, modes):
+        operand1 = self.read_memory(self.memory[self.pc + 1], modes[0])
+        operand2 = self.read_memory(self.memory[self.pc + 2], modes[1])
+        loc = self.memory[self.pc + 3]
+        self.memory[loc] = 1 if operand1 < operand2 else 0
+        return self.pc + 4
 
-def op_less_than(memory, modes, pc, inputs, outputs):
-    operand1 = read_memory(memory, memory[pc + 1], modes[0])
-    operand2 = read_memory(memory, memory[pc + 2], modes[1])
-    loc = memory[pc + 3]
-    memory[loc] = 1 if operand1 < operand2 else 0
-    return pc + 4
+    def op_equals(self, modes):
+        operand1 = self.read_memory(self.memory[self.pc + 1], modes[0])
+        operand2 = self.read_memory(self.memory[self.pc + 2], modes[1])
+        loc = self.memory[self.pc + 3]
+        self.memory[loc] = 1 if operand1 == operand2 else 0
+        return self.pc + 4
 
+    def op_halt(self, modes):
+        raise HaltException()
 
-def op_equals(memory, modes, pc, inputs, outputs):
-    operand1 = read_memory(memory, memory[pc + 1], modes[0])
-    operand2 = read_memory(memory, memory[pc + 2], modes[1])
-    loc = memory[pc + 3]
-    memory[loc] = 1 if operand1 == operand2 else 0
-    return pc + 4
+    def run(self):
+        """
+      >>> computer = IntcodeComputer([1, 0, 0, 2, 99])
+      >>> computer.run()
+      >>> computer.memory
+      [1, 0, 2, 2, 99]
+      >>> computer = IntcodeComputer([1,9,10,3,2,3,11,0,99,30,40,50])
+      >>> computer.run()
+      >>> computer.memory
+      [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+      >>> computer = IntcodeComputer([1,0,0,0,99])
+      >>> computer.run()
+      >>> computer.memory
+      [2, 0, 0, 0, 99]
+      >>> computer = IntcodeComputer([2,3,0,3,99])
+      >>> computer.run()
+      >>> computer.memory
+      [2, 3, 0, 6, 99]
+      >>> computer = IntcodeComputer([2,4,4,5,99,0])
+      >>> computer.run()
+      >>> computer.memory
+      [2, 4, 4, 5, 99, 9801]
+      >>> computer = IntcodeComputer([1,1,1,4,99,5,6,0,99])
+      >>> computer.run()
+      >>> computer.memory
+      [30, 1, 1, 4, 2, 5, 6, 0, 99]
+      """
+        while True:
+            intcode = self.memory[self.pc]
 
+            try:
+                opcode = intcode % 100
+                modes = parse_modes(intcode // 100)
+                op = self.operations[opcode]
+                self.pc = op(modes)
 
-def op_halt(memory, modes, pc, inputs, outputs):
-    raise HaltException()
+            except HaltException:
+                return
 
-
-operations = {
-    1: op_add,
-    2: op_mul,
-    3: op_load,
-    4: op_print,
-    5: op_jump_if_true,
-    6: op_jump_if_false,
-    7: op_less_than,
-    8: op_equals,
-    99: op_halt,
-}
-
-
-def process(memory, inputs=[]):
-    """
-    >>> process([1, 0, 0, 2, 99])
-    ([1, 0, 2, 2, 99], [])
-    >>> process([1,9,10,3,2,3,11,0,99,30,40,50])
-    ([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50], [])
-    >>> process([1,0,0,0,99])
-    ([2, 0, 0, 0, 99], [])
-    >>> process([2,3,0,3,99])
-    ([2, 3, 0, 6, 99], [])
-    >>> process([2,4,4,5,99,0])
-    ([2, 4, 4, 5, 99, 9801], [])
-    >>> process([1,1,1,4,99,5,6,0,99])
-    ([30, 1, 1, 4, 2, 5, 6, 0, 99], [])
-    """
-    memory = memory[:]
-    pc = 0
-    outputs = []
-
-    while True:
-        intcode = memory[pc]
-
-        try:
-            opcode = intcode % 100
-            modes = parse_modes(intcode // 100)
-            op = operations[opcode]
-            pc = op(memory, modes, pc, inputs, outputs)
-
-        except HaltException:
-            return (memory, outputs)
-
-        except KeyError:
-            raise Exception("Unexpected intcode: %s" % intcode)
+            except KeyError:
+                raise Exception("Unexpected intcode: %s" % intcode)
 
 
 def calculate_thruster(program, phase_sequence):
     """
-    >>> program = read_input('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0')
+    >>> program = program = read_input('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0')
     >>> phase_sequence = '43210'
     >>> calculate_thruster(program, phase_sequence)
     43210
@@ -166,14 +172,28 @@ def calculate_thruster(program, phase_sequence):
     >>> calculate_thruster(program, phase_sequence)
     65210
     """
-    input_signal = 0
+
+    computer_threads = []
+    ultimate_input_queue = queue.Queue()
+    input_queue = ultimate_input_queue
 
     for amp in range(len(phase_sequence)):
         phase = int(phase_sequence[amp])
-        _, outputs = process(program, [phase, input_signal])
-        input_signal = outputs[0]
+        input_queue.put(phase)
+        output_queue = queue.Queue()
 
-    return input_signal
+        computer = IntcodeComputer(program, input_queue, output_queue)
+        thread = threading.Thread(target=computer.run)
+        computer_threads.append(thread)
+
+        input_queue = output_queue
+
+    ultimate_input_queue.put(0)
+
+    for t in computer_threads:
+        t.start()
+
+    return input_queue.get()
 
 
 def optimize_thruster(program):
