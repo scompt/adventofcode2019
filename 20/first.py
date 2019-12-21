d6b89cd89a2eb1018972efe16be599577f8d8ef7
Edward Dale <scompt@scompt.com>
2019-12-21 13:56:58 +0100

    Day 20, Part 2

--- 20/first.py
+++ 20/first.py
@@ -5,7 +5,41 @@ from collections import defaultdict
 from textwrap import dedent
 
 
-def dfs(edges, start, target):
+def ignore_portals_neighbors(edges, portals, node):
+    e = (node[0], node[1])
+    for n in edges[e]:
+        yield (n[0], n[1], 0)
+
+
+def first_portals_neighbors(edges, portals, node):
+    e = (node[0], node[1])
+    for n in edges[e]:
+        yield (n[0], n[1], 0)
+
+    for outer, inner in portals:
+        if e == inner:
+            yield (outer[0], outer[1], 0)
+        elif e == outer:
+            yield (inner[0], inner[1], 0)
+
+
+def second_portals_neighbors(edges, portals, node):
+    e = (node[0], node[1])
+    for n in edges[e]:
+        yield (n[0], n[1], node[2])
+
+    z = node[2]
+    for outer, inner in portals:
+        if e == outer and z > 0:
+            yield (inner[0], inner[1], z - 1)
+        elif e == inner and z < 200:
+            yield (outer[0], outer[1], z + 1)
+
+
+node_neighbors = first_portals_neighbors
+
+
+def dfs(edges, portals, start, target):
     parents = {}
     discovered = set()
     Q = [start]
@@ -23,12 +57,12 @@ def dfs(edges, start, target):
 
         discovered.add(node)
 
-        for edge in edges[node]:
+        for edge in node_neighbors(edges, portals, node):
             if edge in discovered:
                 continue
 
             parents[edge] = node
-            Q.insert(0, edge)
+            Q.append(edge)
 
 
 def read_input(lines):
@@ -54,7 +88,13 @@ def read_input(lines):
     ...              Z
     ...              Z
     ... '''
-    >>> r = read_input(dedent(inp)[1:].rstrip().split('\\n'))
+    >>> edges, portals, start, end = read_input(dedent(inp)[1:].rstrip().split('\\n'))
+    >>> start
+    (9, 2, 0)
+    >>> end
+    (13, 16, 0)
+    >>> portals
+    dict_values([((2, 8), (9, 6)), ((2, 13), (6, 10)), ((2, 15), (11, 12))])
     """
     chars = defaultdict(lambda: " ")
     max_x = max_y = 0
@@ -67,7 +107,7 @@ def read_input(lines):
     height = max_y - 3
 
     edges = defaultdict(lambda: [])
-    portals = defaultdict(lambda: [])
+    portals = defaultdict(lambda: (None, None))
     for y in range(max_y):
         for x in range(max_x):
             loc = (x, y)
@@ -75,7 +115,7 @@ def read_input(lines):
             if char != ".":
                 continue
 
-            for n in neighbors(loc):
+            for n in loc_neighbors(loc):
                 n_loc = tuple(map(operator.add, loc, n))
 
                 if chars[n_loc] == ".":
@@ -83,19 +123,22 @@ def read_input(lines):
                 elif chars[n_loc].isalpha():
                     nn_loc = tuple(map(operator.add, n_loc, n))
                     portal_label = "".join(sorted([chars[n_loc], chars[nn_loc]]))
-                    portals[portal_label].append(loc)
+
+                    if x == 2 or y == 2 or x == max_x - 2 or y == max_y - 2:
+                        portals[portal_label] = (loc, portals[portal_label][1])
+                    else:
+                        portals[portal_label] = (portals[portal_label][0], loc)
 
     start = portals.pop("AA")[0]
     end = portals.pop("ZZ")[0]
 
-    for portal_from, portal_to in portals.values():
-        edges[portal_from].append(portal_to)
-        edges[portal_to].append(portal_from)
+    start = (start[0], start[1], 0)
+    end = (end[0], end[1], 0)
 
-    return edges, start, end
+    return edges, portals.values(), start, end
 
 
-def neighbors(loc):
+def loc_neighbors(loc):
     yield ((+1, 0))
     yield ((-1, 0))
     yield ((0, +1))
@@ -106,5 +149,5 @@ if __name__ == "__main__":
     import sys
 
     lines = sys.stdin.readlines()
-    edges, start, end = read_input(l.rstrip() for l in lines)
-    print(len(dfs(edges, start, end)))
+    edges, portals, start, end = read_input(l.rstrip() for l in lines)
+    print(len(dfs(edges, portals, start, end)))
